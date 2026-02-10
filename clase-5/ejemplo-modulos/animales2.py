b4d4dfccd53fb8b00852bb45daa894102bdcd2d0
Mario Esteban Lopez Campos <mariolopezcampos@gmail.com>
2026-02-10 16:02:43 -0600

    Ejemplos extra clase 5

---
+++ clase-5/ejemplo-modulos/animales2.py
@@ -0,0 +1,8 @@
+def perro():
+    print("El perro 2 dijo: Guau")
+
+def dinosaurio():
+    print("El Gato dijo: Roaarrrr")
+
+def rana():
+    print("La Serpiente dijo: croac")
---
+++ clase-5/ejemplo-modulos/guarderia-animales-import-total-colision.py
@@ -0,0 +1,10 @@
+from animales import *
+from animales2 import *
+
+def imprimir_animales():
+    perro()
+    gato()
+    serpiente()
+    zorro()
+
+imprimir_animales()
