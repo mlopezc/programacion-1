a18b14ec55bb7c21a9ada804488e64b06278da25
Mario Esteban Lopez Campos <mariolopezcampos@gmail.com>
2026-03-17 18:25:46 -0600

    Escritura csv

--- clase-10/formatos-texto-plano/ejemplo-csv.py
+++ clase-10/formatos-texto-plano/ejemplo-csv.py
@@ -1,7 +1,20 @@
 import csv
 
 with open("data/datos.csv", newline="") as f:
-    lector = csv.reader(f)
+    lector = csv.DictReader(f)
+    
 
     for fila in lector:
         print(fila)
+
+
+with open("output/ejemplo.csv", "a",newline="") as csvfile:
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    # writer.writeheader()
+    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+   
+    
